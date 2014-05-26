11dc575d9d737259c755ca3809c3078f3839ff47
nurdin <lowyat_net@yahoo.com>
2014-05-26 18:01:49 +0800

    add worklight adapter

--- apps/CRUDWorklight/iphone/native/www/default/worklight/checksum.js
+++ apps/CRUDWorklight/iphone/native/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":2211255198,"date":1401083107927,"machine":"Mohammads-MacBook-Air.local"};
-/* Date: Mon May 26 13:45:07 MYT 2014 */
+var WL_CHECKSUM = {"checksum":2211255198,"date":1401085834452,"machine":"Mohammads-MacBook-Air.local"};
+/* Date: Mon May 26 14:30:34 MYT 2014 */
